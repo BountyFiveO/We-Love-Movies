ce4daee9b34c4adad58fea2ba8f74399bf2bf02b
S. Patterson <bountyfiveo@gmail.com>
2023-02-21 18:45:32 -0500

    Update knexfile.js
    Updated the process.env and added the database url in the correct spot

--- knexfile.js
+++ knexfile.js
@@ -1,12 +1,12 @@
 const path = require("path");
-
 require("dotenv").config();
 
 const {
-   DEVELOPMENT_DATABASE_URL = "postgres://oerlmerv:TwNTB-n0vsAfll_czxfXvrE5zW9tBXTS@manny.db.elephantsql.com/oerlmerv",
+   DEVELOPMENT_DATABASE_URL = "postgresql://postgres@localhost/postgres",
+   PRODUCTION_DATABASE_URL = "postgresql://postgres@localhost/postgres",
  } = process.env;
 
-
+console.log(process.env.DEVELOPMENT_DATABASE_URL); //You can delete this console.log before production deployment
 module.exports = {
   development: {
     client: "postgresql",
